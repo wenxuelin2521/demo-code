47eaac50a740f77de82ef17d08118df50b005370
wenxl <xuelin.common@gmail.com>
2021-04-08 10:43:30 +0800

    闭包 update

--- interview-practice/闭包.js
+++ interview-practice/闭包.js
@@ -12,8 +12,26 @@
 //     })(i)
 // }
 
-for(var i = 0 ; i < 5 ; i++){
-    setTimeout((j) => {
-        console.log(j);
-    } , i * 1000 , i)
-}
+// for(var i = 0 ; i < 5 ; i++){
+//     setTimeout((j) => {
+//         console.log(j);
+//     } , i * 1000 , i)
+// }
+
+
+var name = "world";
+(function(){
+    console.log(typeof name)
+    if(typeof name === undefined){
+        var name = 'jack'
+        console.log('goodbye ' + name)
+    }else{
+        console.log('hello ' + name)
+    }
+})()
+
+// 结果：hello undefined
+
+// 考点：
+// 1. 变量提升：闭包函数中，有var name的声明提升到函数作用于顶部覆盖了最外层的生命，相当于 var name = undefined
+// 2. undefined === undefined 为true；但是typeof返回的是字符串
