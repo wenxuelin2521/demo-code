ea4431d36a97b9548589d3b6543600d1f9126097
wenxl <xuelin.common@gmail.com>
2021-05-20 11:26:59 +0800

    update taro

---
+++ taroApp/src/app.config.ts
@@ -0,0 +1,12 @@
+export default {
+  pages: [
+    'pages/index/index',
+    'pages/goods-list/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  }
+}
---
+++ taroApp/src/pages/index/index.tsx
@@ -0,0 +1,31 @@
+import { Component } from "react";
+import { View, Text } from "@tarojs/components";
+import Taro from "@tarojs/taro"
+import "./index.scss";
+
+export default class Index extends Component {
+  componentWillMount() {}
+
+  componentDidMount() {}
+
+  componentWillUnmount() {}
+
+  componentDidShow() {}
+
+  componentDidHide() {}
+
+  render() {
+    return (
+      <View className="index">
+        <Text onClick={this.test}>Hello world!</Text>
+      </View>
+    );
+  }
+
+  test() {
+    console.log("test", Taro);
+    Taro.navigateTo({
+      url: '/pages/goods-list/index'
+    })
+  }
+}
