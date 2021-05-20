b07a0d2d2035eeb11bec8c2381c27e3a2dc6bba5
wenxl <xuelin.common@gmail.com>
2021-05-20 17:00:08 +0800

    update g6 tree mobile

---
+++ tree-绘制树图/G6/mobile-data.js
@@ -0,0 +1,110 @@
+// 20210520135457
+// https://gw.alipayobjects.com/os/antvdemo/assets/data/modeling-methods.json
+
+var mobiledata = {
+  name: "Modeling Methods",
+  children: [
+    {
+      name: "Classification",
+      children: [
+        {
+          name: "Logistic regression",
+        },
+        {
+          name: "Linear s",
+        },
+        {
+          name: "Rules",
+        },
+        {
+          name: "Decision trees",
+        },
+        {
+          name: "Naive Bayes",
+        },
+        {
+          name: "K nearest neighbor",
+        },
+        {
+          name: "Probabilistic ",
+        },
+        {
+          name: "Support ",
+        },
+      ],
+    },
+    {
+      name: "Consensus",
+      // children: [
+      //   {
+      //     name: "Models diversity",
+      //     children: [
+      //       {
+      //         name: "Different initializations",
+      //       },
+      //       {
+      //         name: "Different parameter choices",
+      //       },
+      //       {
+      //         name: "Different architectures",
+      //       },
+      //       {
+      //         name: "Different modeling methods",
+      //       },
+      //       {
+      //         name: "Different training sets",
+      //       },
+      //       {
+      //         name: "Different feature sets",
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     name: "Methods",
+      //     children: [
+      //       {
+      //         name: "Classifier selection",
+      //       },
+      //       {
+      //         name: "Classifier fusion",
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     name: "Common",
+      //     children: [
+      //       {
+      //         name: "Bagging",
+      //       },
+      //       {
+      //         name: "Boosting",
+      //       },
+      //       {
+      //         name: "AdaBoost",
+      //       },
+      //     ],
+      //   },
+      // ],
+    },
+    {
+      name: "Regression",
+      // children: [
+      //   {
+      //     name: "Multiple linear regression",
+      //   },
+      //   {
+      //     name: "Partial least squares",
+      //   },
+      //   {
+      //     name: "Multi-layer feedforward neural network",
+      //   },
+      //   {
+      //     name: "General regression neural network",
+      //   },
+      //   {
+      //     name: "Support vector regression",
+      //   },
+      // ],
+    },
+  ],
+};
